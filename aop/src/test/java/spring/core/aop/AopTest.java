eb5587f736e9cbbd3a3d2148180994bc743be435
Sudongk <82927161+Sudongk@users.noreply.github.com>
2023-08-05 20:26:24 +0900

    feat: AOP 프로젝트 실습

--- aop/src/test/java/spring/core/aop/AopTest.java
+++ aop/src/test/java/spring/core/aop/AopTest.java
@@ -6,13 +6,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import spring.core.aop.order.OrderRepository;
 import spring.core.aop.order.OrderService;
+import spring.core.aop.order.aop.AspectV6Advice;
 
 import static org.assertj.core.api.Assertions.*;
 
 @Slf4j
 @SpringBootTest
+@Import(AspectV6Advice.class)
 public class AopTest {
 
     @Autowired
